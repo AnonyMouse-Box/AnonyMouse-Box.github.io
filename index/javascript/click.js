38af39174627cef4122d57a68cb82b69a6a6f268
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-09-25 13:00:35 +0100

    Update click.js

--- index/javascript/click.js
+++ index/javascript/click.js
@@ -1,6 +1,7 @@
 const progress = (id) => {
   if(document.getElementById(id).value === 100){
     window.alert('Complete!');
+    document.getElementById(id).value = 0;
   } else {
     document.getElementById(id).value ++;
     document.getElementById(id).title = document.getElementById(id).value;

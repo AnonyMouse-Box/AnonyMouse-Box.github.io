253fc0e6b8980f45884059e06e32a581fcb5db5f
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-09-28 12:46:31 +0100

    Update click.js

--- index/javascript/click.js
+++ index/javascript/click.js
@@ -2,6 +2,7 @@ const progress = (id) => {
   if(document.getElementById(id).value === 100){
     window.alert('Complete!');
     document.getElementById(id).value = 0;
+    heart.classList.remove('animated');
   } else {
     document.getElementById(id).value ++;
     document.getElementById(id).title = document.getElementById(id).value;

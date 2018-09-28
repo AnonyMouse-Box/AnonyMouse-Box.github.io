50c9ed048b340fc9fe3f52f64c9c4432ba7260e4
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-09-28 12:55:12 +0100

    Update click.js

--- index/javascript/click.js
+++ index/javascript/click.js
@@ -1,4 +1,4 @@
-var circle = document.getElementsByClassName('circle')[1]
+var circle = document.getElementsByClassName('circle')
 
 const progress = (id) => {
   if(document.getElementById(id).value === 100){

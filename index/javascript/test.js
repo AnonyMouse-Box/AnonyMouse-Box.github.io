46246b60d8e0b2193332a51b8a52e41167f92b99
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-11-13 22:42:44 +0000

    Update test.js

--- index/javascript/test.js
+++ index/javascript/test.js
@@ -5,5 +5,5 @@ const hide = () => {
 
 const unhide = () => {
   document.getElementsByClassName("unhide")[0].setAttribute("id", "hide");
-  document.getElementsByClassName("nav")[0].removeAttribute("id");
+  document.getElementByTagName("nav")[0].removeAttribute("id");
 }

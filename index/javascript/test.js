b2077382750eddd289399a3cc700364d70d29ba9
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-11-29 17:23:17 +0000

    Update test.js

--- index/javascript/test.js
+++ index/javascript/test.js
@@ -1,5 +1,6 @@
 const hide = () => {
   document.getElementsByClassName("unhide")[0].removeAttribute("id");
+  document.getElementsByClassName("hide")[0].setAttribute("id", "disappear");
   document.getElementsByTagName("nav")[0].setAttribute("id", "collapse");
   document.getElementsByClassName("title")[0].setAttribute("id", "hide");
   document.getElementsByClassName("quote")[0].setAttribute("id", "hide");
@@ -8,7 +9,8 @@ const hide = () => {
 }
 
 const unhide = () => {
-  document.getElementsByClassName("unhide")[0].setAttribute("id", "hide");
+  document.getElementsByClassName("unhide")[0].setAttribute("id", "disappear");
+  document.getElementsByClassName("hide")[0].removeAttribute("id");
   document.getElementById("nav")[0].removeAttribute("id");
   document.getElementsByClassName("title")[0].removeAttribute("id");
   document.getElementsByClassName("quote")[0].removeAttribute("id");

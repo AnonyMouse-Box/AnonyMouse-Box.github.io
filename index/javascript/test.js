1350d2acd94f37df94a7239e5e2d030436f25287
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-11-29 14:20:48 +0000

    Update test.js

--- index/javascript/test.js
+++ index/javascript/test.js
@@ -1,5 +1,6 @@
 const hide = () => {
   document.getElementsByTagName("nav")[0].setAttribute("id", "collapse");
+  document.getElementsByID("nav")[0].setAttribute("id", "hide");
   document.getElementsByClassName("content")[0].setAttribute("id", "hide-top");
   document.getElementsByClassName("unhide")[0].removeAttribute("id");
 }
@@ -8,4 +9,5 @@ const unhide = () => {
   document.getElementsByClassName("unhide")[0].setAttribute("id", "hide");
   document.getElementsByClassName("content")[0].removeAttribute("id");
   document.getElementsByTagName("nav")[0].removeAttribute("id");
+  document.getElementsByID("nav")[0].removeAttribute("id");
 }

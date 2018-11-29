0ae0a20b37304e06c074a90b780a3f45d8356429
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-11-29 21:16:00 +0000

    Update test.js

--- index/javascript/test.js
+++ index/javascript/test.js
@@ -28,9 +28,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementsByClassName("top").style.display = "block";
-    } else {
         document.getElementsByClassName("top").style.display = "none";
+    } else {
+        document.getElementsByClassName("top").style.display = "block";
     }
 }
 

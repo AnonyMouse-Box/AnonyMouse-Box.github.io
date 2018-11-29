97b77e101f78d9ad5a21371a906bf27b46054651
C.Monkey <36849880+hogwashga@users.noreply.github.com>
2018-11-29 15:20:48 +0000

    Update test.js

--- index/javascript/test.js
+++ index/javascript/test.js
@@ -1,15 +1,15 @@
 const hide = () => {
+  document.getElementsByClassName("unhide")[0].removeAttribute("id");
   document.getElementsByTagName("nav")[0].setAttribute("id", "collapse");
   document.getElementsByClassName("title")[0].setAttribute("id", "hide");
   document.getElementsByClassName("quote")[0].setAttribute("id", "hide");
   document.getElementsByClassName("content")[0].setAttribute("id", "hide-top");
-  document.getElementsByClassName("unhide")[0].removeAttribute("id");
 }
 
 const unhide = () => {
   document.getElementsByClassName("unhide")[0].setAttribute("id", "hide");
-  document.getElementsByClassName("content")[0].removeAttribute("id");
   document.getElementById("nav")[0].removeAttribute("id");
   document.getElementsByClassName("title")[0].removeAttribute("id");
   document.getElementsByClassName("quote")[0].removeAttribute("id");
+  document.getElementsByClassName("content")[0].removeAttribute("id");
 }
